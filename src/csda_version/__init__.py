a2e0d21fd1a5a6694a15567ffb903545ca6cd0de
Pete Gadomski <pete.gadomski@gmail.com>
2026-01-20 13:37:36 -0700

    feat: use pydantic

--- src/csda_version/__init__.py
+++ src/csda_version/__init__.py
@@ -1,25 +1,108 @@
+from __future__ import annotations
+
 import os
 import sys
+from typing import Literal
+
+from pydantic import BaseModel, Field
 
 
-def get_next_version(csda_version: str, tag: str) -> str:
+class CsdaVersion(BaseModel):
+    year: int = Field(gt=2000, lt=2100)
+    pi: Literal[1, 2, 3, 4]
+    sprint: int
+
+    @classmethod
+    def parse(cls, s: str) -> CsdaVersion:
+        parts = s.split(".")
+        if len(parts) != 3:
+            raise ValueError(f"Invalid CSDA version: {s}")
+        return CsdaVersion.model_validate(
+            {"year": int("20" + parts[0]), "pi": int(parts[1]), "sprint": parts[2]}
+        )
+
+    def to_str(self) -> str:
+        return f"{str(self.year)[2:]}.{self.pi}.{self.sprint}"
+
+
+class Tag(CsdaVersion):
+    release: int
+
+    @classmethod
+    def parse(cls, s: str) -> Tag:
+        if not s.startswith("v"):
+            raise ValueError(f"Invalid tag: {s}")
+        parts = s.split(".")
+        if len(parts) == 3 and "-" in parts[2]:
+            parts = parts[0:2] + parts[2].split("-")
+        if len(parts) != 4:
+            raise ValueError(f"Invalid tag: {s}")
+        return Tag.model_validate(
+            {
+                "year": int("20" + parts[0][1:]),
+                "pi": int(parts[1]),
+                "sprint": parts[2],
+                "release": parts[3],
+            }
+        )
+
+    def to_str(self) -> str:
+        csda_version_str = super().to_str()
+        return csda_version_str + "-" + str(self.release)
+
+
+def get_next_version(csda_version_str: str, tag_str: str) -> str:
     """Calculate the next version based on the given tag."""
-    csda_version_parts = csda_version.split(".")
-    if tag.startswith("v"):
-        tag_parts = tag[1:].split(".")
-        if "-" in tag_parts[2]:
-            tag_parts = tag_parts[0:2] + tag_parts[2].split("-")
+    csda_version = CsdaVersion.parse(csda_version_str)
+    if tag_str:
+        tag = Tag.parse(tag_str)
     else:
-        tag_parts = None
+        tag = None
     if (
-        tag_parts
-        and csda_version_parts[0] == tag_parts[0]
-        and csda_version_parts[1] == tag_parts[1]
-        and csda_version_parts[2] == tag_parts[2]
+        tag
+        and csda_version.year == tag.year
+        and csda_version.pi == tag.pi
+        and csda_version.sprint == tag.sprint
     ):
-        return ".".join(tag_parts[0:3]) + "-" + str(int(tag_parts[3]) + 1)
+        tag.release += 1
+        return tag.to_str()
     else:
-        return ".".join(csda_version_parts) + "-0"
+        return Tag(
+            year=csda_version.year,
+            pi=csda_version.pi,
+            sprint=csda_version.sprint,
+            release=0,
+        ).to_str()
+
+
+def get_next_csda_version(date: str, csda_version_str: str) -> str:
+    """Returns the next CSDA version."""
+    date_parts = date.split("-")
+
+    year = int(date_parts[0])
+    month = int(date_parts[1])
+    if month <= 3:
+        pi = 2
+    elif month <= 6:
+        pi = 3
+    elif month <= 9:
+        pi = 4
+    else:
+        pi = 1
+    if pi == 1:
+        year += 1
+
+    csda_version = CsdaVersion.parse(csda_version_str)
+    if year != csda_version.year:
+        csda_version.year = year
+        csda_version.pi = 1
+        csda_version.sprint = 0
+    elif pi != csda_version.pi:
+        csda_version.pi = pi
+        csda_version.sprint = 0
+    else:
+        csda_version.sprint += 1
+    return csda_version.to_str()
 
 
 if __name__ == "__main__":
--- tests/test_next_version.py
+++ tests/test_next_version.py
@@ -1,6 +1,6 @@
 import pytest
 
-from csda_version import get_next_version
+from csda_version import get_next_csda_version, get_next_version
 
 
 @pytest.mark.parametrize(
@@ -17,3 +17,15 @@ from csda_version import get_next_version
 )
 def test_get_next_version(csda_version: str, tag: str, expected: str) -> None:
     assert get_next_version(csda_version, tag) == expected
+
+
+@pytest.mark.parametrize(
+    "date,csda_version,expected",
+    [
+        ["2026-01-25", "26.2.1", "26.2.2"],
+        ["2026-10-01", "26.4.4", "27.1.0"],
+        ["2027-01-01", "27.1.3", "27.2.0"],
+    ],
+)
+def test_get_next_csda_version(date: str, csda_version: str, expected: str) -> None:
+    assert get_next_csda_version(date, csda_version) == expected
